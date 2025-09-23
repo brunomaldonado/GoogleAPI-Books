11f2c17464f3b13fde06b5f3d2de781ae7eb5ff6
brunomaldonado <brunoutj@gmail.com>
2025-09-23 02:18:21 -0600

    fix main

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class bcolors:
    UNDERLINE = '\033[4m'
 
 emojis = ['📗', '📓', '📕', '📙', '📔']
+gen_emoji = ['👦', '👧']
 
 def print_options():
   print()
@@ -25,7 +26,7 @@ def print_options():
     f" [1] {emo} Books",
     " [2] Search Books",
     " [3] 📇 Library",
-    " [4] Create 🤵🏽🤵🏻‍♀️",
+    " [4] Create User",
     " [5] 📚 Borrowed",
     " [6] Exit..."
   ]
@@ -51,14 +52,11 @@ def main():
     google_search = input("\n Enter 📖 title: ")
     print()
     books = server.get_book(google_search)
-    # print(f"books: {books}\nlen: {len(books)}")
-
     for idx, book in enumerate(books, start=1):
       # print(f"{idx:2} {book['volumeInfo']['title']}")
       data.append(book['volumeInfo'])
       prices.append(book['saleInfo'])
     for idx, book in enumerate(data, start=1):
-      # print(f"[{idx:2}] {indentation_title1(book['title'])}")
       wrapped_lines = textwrap_title(book['title'])
       print(f" [{idx:2}] {wrapped_lines[0].lstrip()}")
       for line in wrapped_lines[1:]:
@@ -74,83 +72,83 @@ def main():
       return None
 
   def book_info():
-    # while True:
-    #   try:
-    selection = int(input("\n Select book [#?]: "))
-    option_book = select_index(selection)
-    # print(f"===OPTION BOOK: {option_book}")
-    book_num = option_book + 1
-
-    # print(f"\noption_book: {option_book}\nbook_num: {book_num}\n")
-
-    if len(library.users) == 0:
-      print(f"\n      You need to register and CREATE a USER\n")
-      # break
-
-    else:
-      if isinstance(option_book, int):
-        get_title = data[option_book]
-        book = Book(get_title['title'])
-        library.book_number.append(book_num)
-        library.add_book(book)
-
-        if library.book_number.count(book_num) > 1 :
-          print(f" This book has been recently added.....!\n\n")
-          # print(f"LIBRARY.BOOKS: {library.books}")
-          library.books.pop()
-          library.book_number.pop()
-          return
-
-        get_book = data[option_book]
-
-        if 'publisher' in get_book:
-          editorial = get_book['publisher']
-        else:
-          editorial = "Campeon"
-        if 'subtitle' in get_book:
-          subtitle = get_book['subtitle']
-        else:
-          subtitle = "Just Because"
-        if 'description' in get_book:
-          description = get_book['description']
-        else:
-          description = " Be happy"
-        if 'authors' in get_book:
-          authors = get_book['authors']
-          author = ", ".join(map(str, authors))
-        else:
-          author = "Rigoberto Bruno Maldonado"
-        if 'publishedDate' in get_book:
-          publication = get_book['publishedDate']
-        else:
-          publication = "2024-07-24"
-        authors = get_book['authors']
-        author = ", ".join(map(str, authors))
-        spacing = " " * 34
-
-        if option_book is not None:
-          if 'listPrice' in prices[option_book]:
-            for_sale = f"{prices[option_book]['listPrice']['amount']} {prices[option_book]['listPrice']['currencyCode']}"
-          else:
-            for_sale = "NOT FOR SALE"
+    while True:
+      try:
+        selection = int(input("\n Select book [#?]: "))
+        option_book = select_index(selection)
+        book_num = option_book + 1
+        if len(library.users) == 0:
+          print(f"\n      You need to register and CREATE a USER\n")
+          break
 
         else:
-          print("Invalid selection")
+          if selection is None:
+            print(" Invalid selection!!!.\n")
+            continue
+          if isinstance(option_book, int):
+            get_title = data[option_book]
+            book = Book(get_title['title'])
+            library.book_number.append(book_num)
+            library.add_book(book)
 
-        print("" * 1, "-" * 53)
-        print(f" Title: {indentation_title2(get_book['title'])}")
-        print(f" Sub Title: {indentation_title3(subtitle)}")
-        print(f" Authors: {textwrap_authors(author)}")
-        print(f" Editorial: {editorial}")
-        print(f" Publication Date : {publication}")
-        print(f"\n{spacing} Price: {for_sale}")
-        print("" * 1, "-" * 53)
-        print()
-        print(f"{textwrap_description(description)}\n\n")
-        #indentation_description(description)
-      # except ValueError:
-      #   print(" Please enter book number!")
-      #   continue
+            if library.book_number.count(book_num) > 1 :
+              print(f" This book has been recently added.....!\n\n")
+              # print(f"LIBRARY.BOOKS: {library.books}")
+              library.books.pop()
+              library.book_number.pop()
+              return
+
+            get_book = data[option_book]
+
+            if 'publisher' in get_book:
+              editorial = get_book['publisher']
+            else:
+              editorial = "Campeon"
+            if 'subtitle' in get_book:
+              subtitle = get_book['subtitle']
+            else:
+              subtitle = "Just Because"
+            if 'description' in get_book:
+              description = get_book['description']
+            else:
+              description = " Be happy"
+            if 'authors' in get_book:
+              authors = get_book['authors']
+              author = ", ".join(map(str, authors))
+            else:
+              author = "Rigoberto Bruno Maldonado"
+            if 'publishedDate' in get_book:
+              publication = get_book['publishedDate']
+            else:
+              publication = "2024-07-24"
+            authors = get_book['authors']
+            author = ", ".join(map(str, authors))
+            spacing = " " * 34
+
+            if option_book is not None:
+              if 'listPrice' in prices[option_book]:
+                for_sale = f"{prices[option_book]['listPrice']['amount']} {prices[option_book]['listPrice']['currencyCode']}"
+              else:
+                for_sale = "NOT FOR SALE"
+
+            else:
+              print("Invalid selection")
+
+            print("" * 1, "-" * 53)
+            print(f" Title: {indentation_title2(get_book['title'])}")
+            print(f" Sub Title: {indentation_title3(subtitle)}")
+            print(f" Authors: {textwrap_authors(author)}")
+            print(f" Editorial: {editorial}")
+            print(f" Publication Date : {publication}")
+            print(f"\n{spacing} Price: {for_sale}")
+            print("" * 1, "-" * 53)
+            print()
+            print(f"{textwrap_description(description)}\n\n")
+            break
+
+      except ValueError:
+        print(" Invalid input, please enter a number.\n")
+        continue
 
   book_info()
 
@@ -162,11 +160,9 @@ def main():
         if num not in seen:
           unique_value.append(num)
           seen.add(num)
-      # print(f"unique_value {unique_value}")
 
       book_set = set(library.book_number)
       selected_set = set(library.selected_number)
-      # print(f"book_set{book_set} --- selected_Set{selected_set}")
       if book_set & selected_set:
         available_books = len(book_set - selected_set)
         print(f"\n {available_books} books available to borrow.\n")
@@ -184,9 +180,6 @@ def main():
 
             user = library.users[0]
             book = library.books[index]
-            # add borrowed books
-            # print(f"BOOK {book}")
-            # print(f"\n You have selected the book number: {selection}\n {book.title}\n INDEX: {index}\n")
             user.borrow_book(book)
 
             if library.selected_number.count(selection) > 1:
@@ -212,10 +205,7 @@ def main():
       option = int(input("\n Enter option: "))
       if option == 1:
         print()
-        # for idx, book in enumerate(data, start=1):
-        #   print(f"[{idx:2}] {indentation_title1(book['title'])}")
         for idx, book in enumerate(data, start=1):
-          # print(f"[{idx:2}] {indentation_title1(book['title'])}")
           wrapped_lines = textwrap_title(book['title'])
           print(f" [{idx:2}] {wrapped_lines[0].lstrip()}")
           for line in wrapped_lines[1:]:
@@ -258,7 +248,7 @@ def main():
         print()
         print("" * 1, "-" * 53)
         print(f" {spacing}BORROWED BOOKS")
-        print(f" User: 🤵🏻‍♀️ {user_name}")
+        print(f" User: {user_name}")
         print("" * 1, "-" * 53)
         print()
 
@@ -270,17 +260,11 @@ def main():
 
         def list_borrowed_books():
           print(" List of Borrowed Books.\n")
-
           books = user.borrowed_books
-
           selected_unique = list(dict.fromkeys(library.selected_number))
-
-          # print(f"\nSELECTED NUMBER: {selected_unique}\nBOOOK NUMBER RETURN: {library.book_number_return}\n")
-
           numbers = [item for item in selected_unique if item not in library.book_number_return]
 
           for idx, (number, book) in enumerate(zip(numbers, books), start=1):
-            # print(f"{bcolors.OKCYAN}{idx:2}{bcolors.ENDC} [{bcolors.OKGREEN}{number:2}{bcolors.ENDC}] {book.title}")
             formatted_title = f"[{bcolors.OKGREEN}{number:2}{bcolors.ENDC}] {book.title}"
             wrapped_lines = textwrap_book(formatted_title)
             print(f" {bcolors.OKCYAN}{idx:2}{bcolors.ENDC} {wrapped_lines[0].lstrip()}")
@@ -312,13 +296,10 @@ def main():
 
               if selection in library.selected_number:
                 index_ = library.selected_number.index(selection)
-                # print(f"INDEX: {index_}")
                 library.book_number_return.append(selection)
 
                 user = library.users[0]
                 book = user.borrowed_books[index_]
-                # print(f"BOOK {book}")
-                # print(f"\n You have selected the book number: {selection}\n {book.title}\n INDEX: {index_}\n")
                 user.return_book(book)
                 # return borrowed books
                 library.books.append(book)
@@ -327,16 +308,11 @@ def main():
                   if num in library.selected_number:
                     library.selected_number.remove(num)
 
-                # if library.book_number_return.count(selection) > 1 :
-                #   print(f" This book has been recently returned.....!\n\n")
-                #   library.book_number_return.pop()
-
                 list_borrowed_books()
               elif selection in library.book_number_return:
                 index_ = library.book_number_return.index(selection)
                 book = user.returned_books[index_]
                 if index_ < len(user.returned_books):
-                  #print(f"The {book.title} is on books index[{index_}]")
                   book.return_book()
                   print(f"\n This book has been recently returned.....!\n\n")
 
